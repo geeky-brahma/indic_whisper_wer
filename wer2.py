3a12214f34d326d7e373ac1773f54475e4eef4da
Brahmananda Sahoo <78798925+geeky-brahma@users.noreply.github.com>
2024-04-27 00:26:05 +0530

    Update wer2.py

--- wer2.py
+++ wer2.py
@@ -5,18 +5,12 @@ import torch
 from jiwer import wer
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
 
-# # Load the ASR model
-# # model_name = "openai/whisper-medium"
-# local_model_path = "odia_models\\whisper-medium-or_alldata_multigpu"
-# model = Wav2Vec2ForCTC.from_pretrained(local_model_path)
-# # model = Wav2Vec2ForCTC.from_pretrained(model_name)
-# # tokenizer = Wav2Vec2CTCTokenizer.from_pretrained(local_model_path)
-# tokenizer = Wav2Vec2Tokenizer.from_pretrained(local_model_path)
+
 
 # Load dataset
 dataset_path = "kathbath\\kathbath"
 languages = os.listdir(dataset_path)
-# print(languages)
+# print(languages)  #debug line
 total_wer = 0
 total_count = 0
 lang_code={'bengali':'bn', 'gujarati':'gu', 'hindi':'hi', 'kannada':'kn', 'malayalam':'ml', 'marathi':'mr', 'odia':'or', 'punjabi':'pa', 'sanskrit':'sa', 'tamil':'ta', 'telugu':'te', 'urdu':'ur'}
@@ -26,21 +20,13 @@ for language in languages:
     print(local_model_path)
     model = Wav2Vec2ForCTC.from_pretrained(local_model_path)
     tokenizer = Wav2Vec2Tokenizer.from_pretrained(local_model_path)
-
-
-
-    # if (language!="odia"):
-    #     print(language)
-    #     continue
-    # language_path = os.path.join(dataset_path, '\\')
-    # language_path = os.path.join(language_path, language)
     language_path = os.path.join(dataset_path, language)
     print(language_path)
     if not os.path.isdir(language_path):
         print('no')
         continue
 
-    audio_path = os.path.join(language_path, "wavs")        #changed (language_path, "audio") -> (language_path, "wavs")
+    audio_path = os.path.join(language_path, "wavs")        
     manifest_path = os.path.join(language_path, "manifest.json")
     print(audio_path)
     print(manifest_path)
